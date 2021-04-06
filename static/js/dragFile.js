830d2e5bff3ed8912b40a2f17cbe6ed084b8e6f1
Vlad <lll-ooo-2003@mail.ru>
2021-04-06 22:24:59 +0400

    profile_form

---
+++ static/js/dragFile.js
@@ -0,0 +1,59 @@
+﻿const dragArea = document.querySelector("#dragArea");
+let file;
+const inputFile = document.querySelector("#input_file") 
+let buttonFile = document.querySelector("#button_file")
+
+function showFile(){
+    const fileType = file.type;
+    const valid = ["image/jpeg", "image/jpg", "image/png"]
+    if(valid.includes(fileType)){
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            const fileURL = fileReader.result
+            const img = `<img src="${fileURL}" alt="">
+            <div class="delite_file"><span class="material-icons">close</span></div>`
+            dragArea.innerHTML = img
+            const div = document.querySelector(".delite_file") 
+            div.addEventListener("click", delite)
+        };
+        fileReader.readAsDataURL(file);
+    }else{
+        dragArea.classList.remove("active");
+    }
+}
+
+function delite(){
+    file = null
+    dragArea.innerHTML = '<span class="material-icons">backup</span><label for="img">Обложка</label><button class="button button_form", id="button_file",  type="button">Загрузить</button>'
+    buttonFile = document.querySelector("#button_file")
+    buttonFile.onclick = ()=>{
+        inputFile.click();
+        
+    };
+}
+
+buttonFile.onclick = ()=>{
+    inputFile.click();
+};
+
+inputFile.addEventListener("change", function(){
+    file = this.files[0];
+    showFile()
+    dragArea.classList.remove("active");
+});
+
+dragArea.addEventListener("dragover", (e)=>{
+    e.preventDefault()
+    dragArea.classList.add("pulse")
+});
+
+dragArea.addEventListener("dragleave", ()=>{
+    dragArea.classList.remove("pulse")
+})
+
+dragArea.addEventListener("drop", (e)=>{
+    e.preventDefault()
+    file = e.dataTransfer.files[0];
+    dragArea.classList.remove("pulse")
+    showFile()
+})
---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+﻿const menu_up = document.querySelector(".header_menu")
+const menu_down = document.querySelector(".button_close")
+
+
+menu_up.addEventListener("click", (e)=>{
+    const body_menu = document.querySelector(".menu_back") 
+    body_menu.style.display = "flex"
+    body_menu.style.opacity = "1"
+})
+
+menu_down.addEventListener("click", (e)=>{
+    const body_menu = document.querySelector(".menu_back") 
+    body_menu.style.opacity = "0"
+    body_menu.style.display = "none"
+})
+
+
+
+const model_wind_button = document.querySelector("#button_model")
+const model_wind = document.querySelector("#model")
+
+model_wind_button.addEventListener("click", (e)=>{
+    model_wind.classList.add("model_activ");
+})
+
+model_wind.addEventListener("click", (e)=>{
+    if(e.target == model_wind){
+        model_wind.classList.remove("model_activ");
+    }
+})
